8b136fd6e3f1cab9adc59233814218d21ee814a5
khairulharu <khairulaswad719@gmail.com>
2024-09-04 14:43:46 +0800

    learn penurunan function, kalau di golang dependency injection

---
+++ test/inheritance.test.ts
@@ -0,0 +1,35 @@
+describe('Inheritance', function () {
+     class Repository {
+          service: string;
+
+          constructor(service: string) {
+               this.service = service
+          }
+
+          getNameByUserId(id: number): string {
+               return `We ${id} Found from ${this.service}`;
+          } 
+     }
+
+     class Market extends Repository {}
+
+     class Shipping extends Market {}
+
+     it('should can get extend class', function () {
+
+          const marketRepository: Market = new Market("Market");
+
+          expect(marketRepository.getNameByUserId(3)).toBe("We 3 Found from Market");
+
+          const shippingRepository: Shipping = new Market("Shipping");
+
+          expect(shippingRepository.getNameByUserId(5)).toBe("We 5 Found from Shipping");
+
+          console.info(marketRepository.service);
+          console.info(marketRepository.getNameByUserId(4));
+
+          console.info(shippingRepository.service);
+          console.info(shippingRepository.getNameByUserId(7));
+
+     });
+});
