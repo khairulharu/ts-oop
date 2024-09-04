24f97a0bd2c826a006d481e6705690427074e7b7
khairulharu <khairulaswad719@gmail.com>
2024-09-04 17:31:11 +0800

    change inheritance method

--- test/inheritance.test.ts
+++ test/inheritance.test.ts
@@ -6,35 +6,45 @@ describe('Inheritance', function () {
                this.service = service
           }
 
-          getNameByUserId(id: number): string {
-               return `We ${id} Found from ${this.service}`;
+          getServiceName(): string {
+               return this.service
           } 
      }
 
-     class Market extends Repository {
-          getCategory(name: string): string {
-               return `Your service ${this.service}, We Found this ${name} from Market`
+     class CategoryOnMarket extends Repository {
+          category: string;
+
+          constructor(service: string, category: string) {
+               super(service);
+               this.category = category;
+          }
+
+          getIfValidateCategoryName(): string {
+               if (this.service == this.category) {
+                    return this.category
+               } else {
+                    return "not Found"
+               }
           }
      }
 
-     class Shipping extends Market {}
+     class Shipping extends CategoryOnMarket {
+          validateShippingOrder(): boolean {
+               if (this.service === this.category) {
+                    return true
+               } else {
+                    return false
+               }
+          }
+     }
 
      it('should can get extend class', function () {
+          const foodRepository = new Repository("food");
 
-          const marketRepository: Market = new Market("Market");
+          const foodCategory = new CategoryOnMarket(foodRepository.getServiceName(), "food")
 
-          expect(marketRepository.getNameByUserId(3)).toBe("We 3 Found from Market");
+          const shippingProduct = new Shipping(foodRepository.service, foodCategory.category)
 
-          const shippingRepository: Shipping = new Market("Shipping");
-
-          expect(shippingRepository.getNameByUserId(5)).toBe("We 5 Found from Shipping");
-
-          console.info(marketRepository.service);
-          console.info(marketRepository.getNameByUserId(4));
-
-          console.info(shippingRepository.service);
-          console.info(shippingRepository.getNameByUserId(7));
-
-          console.info(shippingRepository.getCategory("shoe"));
+          expect(shippingProduct.validateShippingOrder()).toBe(true)
      });
 });
