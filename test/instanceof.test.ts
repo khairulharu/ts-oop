71df4d62a0dd9cef63c6a794460a841348326156
khairulharu <khairulaswad719@gmail.com>
2024-09-05 13:55:44 +0800

    learn instance of

--- test/instanceof.test.ts
+++ test/instanceof.test.ts
@@ -1,16 +1,24 @@
 describe('InstanceOf', function () {
+     class Employee {}
+
+     class Manager {}
+
+
+     const budi = new Employee();
+
+     const riki = new Manager();
+
      it('should have problem using typeof', function () {
-          class Employee {}
-     
-          class Manager {}
-
-          const budi = new Employee();
-
-          const riki = new Manager();
-
-          console.info(budi instanceof Employee)
-          console.info(riki instanceof Manager)
+          console.info(typeof budi)
+          console.info(typeof riki)
      });
 
+     it('should suport instanceof', function () {
+          expect(budi instanceof Manager).toBe(false)
+          expect(budi instanceof Employee).toBe(true)
+
+          expect(riki instanceof Employee).toBe(false)
+          expect(riki instanceof Manager).toBe(true)
+     });
 
 });
