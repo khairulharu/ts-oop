303b32957c743798d9ba06e20e78fb56b94ff5d3
khairulharu <khairulaswad719@gmail.com>
2024-09-05 15:56:21 +0800

    wrong polymophism casting data

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -14,7 +14,29 @@ describe('Polymorphism', function () {
      class VicePresident extends Manager {
           
      }
-     
+
+     //ini adalah contoh casting data yang salah
+     function sayHelloWrong(employee: Employee): void {
+          if (employee instanceof Manager) {
+               const manager = employee as Manager
+               console.info(`Hello wrong manager ${manager.name}`);
+          } else if (employee instanceof VicePresident) {
+               const vp = employee as VicePresident
+               console.info(`Hello wrong vp ${vp.name}`)
+          } else {
+               console.info(`Hello wrong employee ${employee.name}`);
+          }
+     }
+
+     //Harus di ingat jika ingin casting data ke tipe data lain
+     //pengecekannya itu harus di mulai dari bawah
+     //karena kalau tidak dia akan salah cek
+     //dikarenaknakan ada sebuah tipe misalknan vice precident di cek
+     //sebgaia instance of mnagaer itu true
+     //karena vicePresident itu extends atau menggunakan
+     //class managaer jadi dia di anggap true
+
+
      function sayHello(employee: Employee): void {
           if (employee instanceof VicePresident) {
                const vp = employee as VicePresident
@@ -50,5 +72,11 @@ describe('Polymorphism', function () {
           sayHello(alok);
           sayHello(layla);
           sayHello(gusion);
+     });
+
+     it('should wrong using parameter', function () {
+          sayHelloWrong(new VicePresident("jiluan"))
+          sayHelloWrong(new Manager("alok gaming"))
+          sayHelloWrong(new Employee("alok jumshot"))
      });
 });
